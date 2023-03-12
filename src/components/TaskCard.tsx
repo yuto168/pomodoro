171d62e3b7c0c2b46aa08e5be4a6e012e2e388ad
yuto <kanawo202090@gmail.com>
2023-03-13 00:53:14 +0900

    タスクアイテムのスタイル修正

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 import { ContextMenu } from "primereact/contextmenu";
 import { VscEdit } from "react-icons/vsc";
-import DialogForEdit from "./ui-parts/EditTaskModal";
+import DialogForEdit from "./ui-parts/DialogForEdit";
 import { TaskItem } from "src/typings/taskItem";
+import { GlassItem } from "./ui-parts/GlassItem";
 
 type props = {
   task: TaskItem;
@@ -15,20 +16,32 @@ type CustomProps = {
   isDragging?: boolean;
 };
 
-const Edit = styled(VscEdit)``;
+const Item = styled(GlassItem)<CustomProps>`
+  position: relative;
+  box-shadow: none;
+  opacity: ${(props) => (props.isDragging ? 0 : 1)};
+  cursor: move;
+`;
 
 const IconGroup = styled.div`
   display: flex;
   cursor: pointer;
+  position: absolute;
+  right: 0;
 `;
-const Item = styled.div<CustomProps>`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #e0edf8;
-  margin: 4%;
-  opacity: ${(props) => (props.isDragging ? 0 : 1)};
-  cursor: move;
+
+const Edit = styled(VscEdit)`
+  padding: 1%;
+  font-size: 1.3rem;
+  color: rgba(55, 53, 47, 0.65);
+  &:hover {
+    backdrop-filter: blur(5px);
+  }
+`;
+
+const Contents = styled.span`
+  color: white;
+  overflow-wrap: anywhere;
 `;
 
 function TaskCard(props: props) {
