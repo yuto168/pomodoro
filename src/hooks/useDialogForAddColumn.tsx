516f9d7d31200f6dd962a3dcdeb0230187ba3129
yuto <kanawo202090@gmail.com>
2023-03-13 00:47:08 +0900

    不要な処理のため削除

--- src/hooks/useDialogForAddColumn.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export default function useDialogForAddColumn() {
-  const [showModal, setShowModal] = useState(false);
-  return { showModal, setShowModal };
-}
