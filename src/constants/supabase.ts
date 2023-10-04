9a9200d58bdf29e92c32e7c79d660b8b4597b711
yuto <kanawo202090@gmail.com>
2023-10-04 14:49:09 +0900

    supabaseキーを環境変数で持つように修正

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -3,7 +3,7 @@ import type { Database } from "src/typings/database";
 
 export const supabase = createClient<Database>(
   // Project　URL
-  "https://pydzqipvxuprutsuduyc.supabase.co",
+  process.env.REACT_APP_SUPABASE_URL!,
   // anon-key
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZHpxaXB2eHVwcnV0c3VkdXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk3NDc2OTUsImV4cCI6MTk5NTMyMzY5NX0.cODFuCZk9wPvtdEmILBNqfgBMfaI6grnAe76HCbR9g4"
+  process.env.REACT_APP_SUPABASE_KEY!
 );
