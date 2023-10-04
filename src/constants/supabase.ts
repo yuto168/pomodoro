2bcb59e01bab70b00e7bb5deffc184c8349d217b
yuto <kanawo202090@gmail.com>
2023-10-04 18:14:31 +0900

    test

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "src/typings/database";
 
+console.log(process.env.REACT_APP_SUPABASE_URL);
+console.log(process.env.REACT_APP_SUPABASE_KEY);
+
 export const supabase = createClient<Database>(
   process.env.REACT_APP_SUPABASE_URL!,
   process.env.REACT_APP_SUPABASE_KEY!
